55563db13a84ce55c27f64d14bad900b89480622
omnicoder <ffmpeg@gmail.com>
2018-10-07 23:52:01 +0800

    Initial commit: Go example 001

---
+++ hello.go
@@ -0,0 +1,23 @@
+//
+// Firstly, setup GOROOT and GOPATH environment variables
+//
+// --------------------------------
+// How to compile?
+// go build
+//
+// How to install?
+// go install github.com/nxtreaming/Hello
+//
+// How to remove?
+// go clean -i github.com/nxtreaming/Hello
+//
+// ---------------------------------
+//
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello world.")
+}
