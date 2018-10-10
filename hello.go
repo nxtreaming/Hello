c5dd5553434084b7b5423f2cf3125d38e19da491
omnicoder <ffmpeg@gmail.com>
2018-10-10 12:52:09 +0800

    Demo: multi-returned value function

--- hello.go
+++ hello.go
@@ -35,16 +35,26 @@ import (
 //	return x+y
 //}
 
-func my_add(x int, y int) int {
+//func my_add(x int, y int) int {
+func my_add(x, y int) int {
 	return x + y
+}
+
+// multi-returned values
+func string_swap(str1, str2 string) (string, string) {
+	return str2, str1
 }
 
 func main() {
 	x := 100
 	y := 200
+	str1, str2 := string_swap("me", "Exchange")
+
+	z := my_add(x, y)
 
 	fmt.Println("The Pi value:", math.Pi)
-	fmt.Println("the sum of is:", my_add(x, y))
+	fmt.Println("the sum of is:", z)
+	fmt.Println(str1, str2)
 	fmt.Println("Hello world.")
 	fmt.Println((stringutil.Reverse(".dlrow olleH")))
 }
