00d7b40b922bc09f44c404fdcf9bf8bd5b8697a9
omnicoder <ffmpeg@gmail.com>
2018-10-10 12:17:18 +0800

    Demo: export variable and function definition

--- hello.go
+++ hello.go
@@ -22,13 +22,29 @@ package main
 
 import (
 	"fmt"
+	"math"
 
 	"github.com/nxtreaming/stringutil"
 )
 
+//
+// 'int {' MUST be in the same line with 'func'
+//
+//func my_add(x int, y int) int
+//{
+//	return x+y
+//}
+
+func my_add(x int, y int) int {
+	return x + y
+}
+
 func main() {
-	//	i := 100
-	//	fmt.Println("The value:", i)
+	x := 100
+	y := 200
+
+	fmt.Println("The Pi value:", math.Pi)
+	fmt.Println("the sum of is:", my_add(x, y))
 	fmt.Println("Hello world.")
 	fmt.Println((stringutil.Reverse(".dlrow olleH")))
 }
