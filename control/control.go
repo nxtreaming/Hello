b8223694228a3531c5198c5c0214fb2d0f8db882
omnicoder <ffmpeg@gmail.com>
2018-10-11 22:51:09 +0800

    Demo: loop cosmetics

--- control/control.go
+++ control/control.go
@@ -13,7 +13,31 @@ func sum(v int) int {
 	return s
 }
 
+func sum2(v int) int {
+	s := 0
+	i := 1
+	for ; i <= v; i++ {
+		s += i
+	}
+
+	return s
+}
+
+func sum3(v int) int {
+	s := 0
+	i := 1
+	// same as while loop
+	for i <= v {
+		s += i
+		i++
+	}
+
+	return s
+}
+
 func main() {
 	x := 100
 	fmt.Printf("The sum of from 1 to %d is %d\n", x, sum(x))
+	fmt.Printf("The sum of from 1 to %d is %d\n", x, sum2(x))
+	fmt.Printf("The sum of from 1 to %d is %d\n", x, sum3(x))
 }
