164177beb9fe94e486522a76b0bd1aa7366a1948
omnicoder <ffmpeg@gmail.com>
2018-10-12 17:17:35 +0800

    for and if statement exercise

--- control/control.go
+++ control/control.go
@@ -64,6 +64,28 @@ func pow(x, n, lim float64) float64 {
 	return lim
 }
 
+func my_sqrt(x float64) float64 {
+	var z float64 = 1.0
+
+	for i := 0; i < 10; i++ {
+		z -= (z*z - x) / (2 * z)
+		fmt.Printf("loop %d: fake sqrt(%v): %v\n", i+1, x, z)
+	}
+
+	return z
+}
+
+func my_sqrt2(x float64) float64 {
+	var z float64 = x / 2
+
+	for i := 0; i < 10; i++ {
+		z -= (z*z - x) / (2 * z)
+		fmt.Printf("loop %d: fake sqrt(%v): %v\n", i+1, x, z)
+	}
+
+	return z
+}
+
 func main() {
 	x := 100
 	fmt.Printf("The sum of from 1 to %d is %d\n", x, sum(x))
@@ -74,4 +96,9 @@ func main() {
 
 	fmt.Printf("The limit power: limit %v, %v^%v = %v\n", 10, 3, 2, pow(3, 2, 10))
 	fmt.Printf("The limit power: limit %v, %v^%v = %v\n", 20, 3, 3, pow(3, 3, 20))
+
+	my_sqrt(10)
+	my_sqrt2(10)
+
+	fmt.Printf("The official sqrt(%d) = %v\n", 10, math.Sqrt(10))
 }
